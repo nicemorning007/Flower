8549b4891772a3e28160683d286465b3c6fc85e0
Nicemorning <oypebook@outlook.com>
2018-03-14 16:51:16 +0800

    Project Closed

---
+++ app/src/main/java/cn/nicemorning/flower/DownActivity.java
@@ -0,0 +1,18 @@
+package cn.nicemorning.flower;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nicemorning on 14-Mar-18.
+ * In package cn.nicemorning.flower
+ */
+
+public class DownActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_down);
+    }
+}
